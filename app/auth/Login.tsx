7857ad363d8ae3844b987e544837f6f381e5bd60
Maya <mayajwong8@gmail.com>
2023-12-22 20:47:25 -0500

    created nav + started auth

---
+++ app/auth/Login.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import {signIn} from 'next-auth/react'
+
+export default function login(){
+    return(
+        <li className="list-none">
+            <button onClick={() => signIn()}className="text-sm">Sign In
+            </button>
+        </li>
+    )
+}
