8f768487030210245b629c1f20f99871d75b2bb1
Maya <mayajwong8@gmail.com>
2023-12-24 23:19:28 -0500

    added google cloud sign in functionality

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -5,7 +5,8 @@ import {signIn} from 'next-auth/react'
 export default function login(){
     return(
         <li className="list-none">
-            <button onClick={() => signIn()}className="text-sm">Sign In
+            <button className="text-sm bg-gray-700 text-white py-2 px-6 round-xl disabled:opacity-25" onClick={() => signIn()}>
+                Sign In
             </button>
         </li>
     )
