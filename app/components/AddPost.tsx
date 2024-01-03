f07d97c22d04fbb3ba46daf76ec9a9dd06a0619a
Maya <mayajwong8@gmail.com>
2024-01-03 01:47:47 -0500

    red text when exceed char count

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -16,7 +16,8 @@ export default function CreatePost(){
                 className="p-4 text-lg rounded-md my-2 bg-gray-200">
                 </textarea>
             </div>
-            <div className={`flex items-center justify-between font-bold`}>
+            <div className={`flex items-center justify-between gap-2`}>
+                <p className={`font-bold text-sm ${title.length > 300 ? "text-red-700" : "text-gray-700"}`}>{`${title.length}/300`}</p>
                 <button
                 disabled={isDisabled}
                 className="text-sm bg-teal-600 text-white py-2 px-4 rounded-xl disabled:opacity-25"
