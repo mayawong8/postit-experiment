e9133347cc7da6a8327efc62bf1ea575c6101b3c
Maya <mayajwong8@gmail.com>
2024-02-04 15:08:56 -0500

    trying to get comment to work

--- prisma/client.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-//checking if we are in production or not
-//checks if client already exists, if it doesn't instantiate it
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client
-
-export default client
