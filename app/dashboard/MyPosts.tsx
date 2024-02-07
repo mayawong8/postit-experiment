d834f71cf44ade005c7753c21754f6c764833eee
Maya <mayajwong8@gmail.com>
2024-02-07 16:00:00 -0500

    started making edit post

--- app/dashboard/MyPosts.tsx
+++ app/dashboard/MyPosts.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from "react-query"
 import axios from "axios"
 import { AuthPosts } from "../types/AuthPosts"
+import EditPost from "./EditPost"
 
 const fetchAuthPosts = async () => {
     const response = await axios.get("/api/posts/authPosts")
@@ -17,7 +18,16 @@ export default function MyPosts() {
     console.log(data)
     return (
         <div>
-            <h1>Data</h1>
+            {data?.posts?.map((post) => (
+            <EditPost 
+            id={post.id} 
+            key={post.id} 
+            avatar={data.image} 
+            name={data.name} 
+            title={post.title} 
+            comments={post.comments}
+            />
+            ))}
         </div>
     )
 }
